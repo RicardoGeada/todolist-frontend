f7043db7d8142a705a32b4d56853e957dbefaa78
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2024-07-26 12:30:58 +0200

    added login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,14 +6,36 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
-  email : string = '';
-  password : string = '';
+  async login() {
+    const myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json');
 
-  login() {
+    const raw = JSON.stringify({
+      username: this.username,
+      password: this.password,
+    });
+
+    const requestOptions : RequestInit = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow',
+    };
+
+    try {
+      let response = await fetch('http://127.0.0.1:8000/login/', requestOptions);
+      let json = await response.json();
+      localStorage.setItem('token', json.token);
+      // TODO: redirect
+    } catch(e) {
+      console.error(e);
+    }
 
   }
 }
